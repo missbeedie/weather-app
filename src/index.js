b1e8546a72cdef7eef21ebc5f3fdaec13eb0848b
missbeedie <142709759+missbeedie@users.noreply.github.com>
2023-11-04 07:59:39 +0000

    search engine js added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert('yo');
+
+function performSearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#form-search-bar");
+  let currentCityElement = document.querySelector("#current-city");
+  currentCityElement.innerHTML = searchInput.value;
+
+}
+
+
+let searchFormElement = document.querySelector("form");
+searchFormElement.addEventListener("submit", performSearch);
+
+
